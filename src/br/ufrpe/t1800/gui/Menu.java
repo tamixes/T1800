28c0f502eed96fc068a7fedd68f85d84aef70c83
Tamixes <tamixxes@gmail.com>
2017-11-25 20:00:58 -0300

    ~~

--- src/br/ufrpe/t1800/gui/Menu.java
+++ src/br/ufrpe/t1800/gui/Menu.java
@@ -30,127 +30,182 @@ public class Menu {
 			
 			
 			switch(op) {
-			case 1:
-				int muda = 6;
-				while(muda != 6) {
-					this.começo();
-					System.out.println("Menu de Pessoa");
-					System.out.println("1 - Cadastrar"
-							+ "2 - Atualizar"
-							+ "3 - Remover"
-							+ "4 - Buscar"
-							+ "5 - Listar"
-							+ "6 - Sair");
-					int opc = entrada.nextInt();
-					
-					switch(opc) {
-					case 1:{
-						boolean ok = false; 
-						do {
-							entrada.nextLine();
-							System.out.println("Cadastro\n\n");
-							
-							System.out.println("Nome: ");
-							String nome = entrada.nextLine();
-							System.out.println("\nEmail: ");
-							String email = entrada.nextLine();
-							System.out.println("\nTelefone: ");
-							String telefone = entrada.nextLine();
-							
-							Pessoa p = new Pessoa(nome, email, telefone);
-							try {
-								wallet.cadastrarPessoa(p);
-							} catch (Exception e) {
-								// TODO: handle exception
-							}
-							ok = true;
-							System.out.println("Cadastro realizado");
-							
-							
-						} while (ok==false);
-					   	break;
-					
-					}case 2:{
-							entrada.nextLine();
-							System.out.println("Atualizar dados Pessoais\n\n");
-							System.out.println("Infome o nome: ");
-							String busca = entrada.nextLine();
-							
-							Pessoa atualiza = null;
-							try {
-								atualiza = wallet.buscarPessoa(busca);
-							} catch (Exception e) {
-								System.out.println(e.getMessage());
-								e.printStackTrace();
-							}
-							if(atualiza != null) {
-								System.out.println("Preencha os dados\n\n");
+				case 1:
+					int muda = 6;
+					while(muda != 6) {
+						this.começo();
+						System.out.println("Menu de Pessoa");
+						System.out.println("1 - Cadastrar"
+								+ "2 - Atualizar"
+								+ "3 - Remover"
+								+ "4 - Buscar"
+								+ "5 - Listar"
+								+ "6 - Sair");
+						int opc = entrada.nextInt();
+						
+						switch(opc) {
+						case 1:{
+							boolean ok = false; 
+							do {
+								entrada.nextLine();
+								System.out.println("Cadastro\n\n");
+								
 								System.out.println("Nome: ");
-								String novoNome = entrada.nextLine();
-								System.out.println("Email: ");
-								String novoEmail = entrada.nextLine();
-								System.out.println("Telefone: ");
-								String novoTelefone = entrada.nextLine();
+								String nome = entrada.nextLine();
+								System.out.println("\nEmail: ");
+								String email = entrada.nextLine();
+								System.out.println("\nTelefone: ");
+								String telefone = entrada.nextLine();
 								
-								
-								
-								atualiza.setNome(novoNome);
-								atualiza.setEmail(novoEmail);
-								atualiza.setTelefone(novoTelefone);
-								
+								Pessoa p = new Pessoa(nome, email, telefone);
 								try {
-									wallet.atualizarPessoa(atualiza);
+									wallet.cadastrarPessoa(p);
+								} catch (Exception e) {
+									// TODO: handle exception
+								}
+								ok = true;
+								System.out.println("Cadastro realizado");
+								
+								
+							} while (ok==false);
+						   	break;
+						
+						}case 2:{
+								entrada.nextLine();
+								System.out.println("Atualizar dados Pessoais\n\n");
+								System.out.println("Infome o nome: ");
+								String busca = entrada.nextLine();
+								
+								Pessoa atualiza = null;
+								try {
+									atualiza = wallet.buscarPessoa(busca);
 								} catch (Exception e) {
 									System.out.println(e.getMessage());
 									e.printStackTrace();
 								}
-							}else {
-								System.out.println("\nPessoa Invalida");
-							}
-							break;
+								if(atualiza != null) {
+									System.out.println("Preencha os dados\n\n");
+									System.out.println("Nome: ");
+									String novoNome = entrada.nextLine();
+									System.out.println("Email: ");
+									String novoEmail = entrada.nextLine();
+									System.out.println("Telefone: ");
+									String novoTelefone = entrada.nextLine();
+									
+									
+									
+									atualiza.setNome(novoNome);
+									atualiza.setEmail(novoEmail);
+									atualiza.setTelefone(novoTelefone);
+									
+									try {
+										wallet.atualizarPessoa(atualiza);
+									} catch (Exception e) {
+										System.out.println(e.getMessage());
+										e.printStackTrace();
+									}
+								}else {
+									System.out.println("\nPessoa Invalida");
+								}
+								break;
+								
+						}
+						case 3:{
+								entrada.nextLine();
+								System.out.println("Remover Pessoa");
+								System.out.println("\nInfome o nome da pesoa: ");
+								String busca = entrada.nextLine();
+								
+								Pessoa apaga = null;
+								try {
+									apaga = wallet.buscarPessoa(busca);
+								} catch (Exception e) {
+									// TODO: handle exception
+								}
+									if(apaga != null) {
+										System.out.println("Deseja deletar " + apaga.getNome() +"?\n"
+												+ "1 - Sim\n"
+												+ "2 - Nao\n");
+										int d = entrada.nextInt();
+										switch(d) {
+										case 1: {
+											try {
+												wallet.removerPessoa(apaga);
+											} catch (Exception e) {
+												// TODO: handle exception
+											}
+											System.out.println("Pessoa removida\n");
+										}
+										}
+											
+									}else {
+										System.out.println("Pessoa nao existe\n");
+									}
 							
-					}
-					case 3:{
-							entrada.nextLine();
-							System.out.println("Remover Pessoa");
-							System.out.println("\nInfome o nome da pesoa: ");
+						}
+						break;
+						
+						case 4:{
+							entrada.nextLine(); 
+							System.out.println("Busca de Pessoa");
+							System.out.println("Nome da pessoa: ");
 							String busca = entrada.nextLine();
 							
-							Pessoa apaga = null;
+							Pessoa encontra = null;
 							try {
-								apaga = wallet.buscarPessoa(busca);
+								encontra = wallet.buscarPessoa(busca);
 							} catch (Exception e) {
 								// TODO: handle exception
 							}
-								if(apaga != null) {
-									System.out.println("Deseja deletar " + apaga.getNome() +"?\n"
-											+ "1 - Sim\n"
-											+ "2 - Nao\n");
-									int d = entrada.nextInt();
-									switch(d) {
-									case 1: {
-										try {
-											wallet.removerPessoa(apaga);
-										} catch (Exception e) {
-											// TODO: handle exception
-										}
-										System.out.println("Pessoa removida\n");
-									}
-									}
-										
-								}else {
-									System.out.println("Pessoa nao existe\n");
-								}
+							if(encontra != null) {
+								System.out.println("Dados: " + encontra.getNome());
+								System.out.println(encontra);
+							}else {
+								System.out.println("Pessoa nao existe!!");
+							}
+							
+						}
+						break;	
 						
+						case 5:{
+							entrada.nextLine();
+							System.out.println("Lista de Pessoas");
+						}
+						break;
+						
+						case 6:{
+							System.out.println("Saindo...");
+							entrada.nextLine();
+							muda = 6;
+							break;
 					}
-					break;
-						
-					
 				}
 			}
+				break;
 			
-			
-			
+				case 2:
+					int muda2 = 0;
+					while(muda2 != 6) {
+						this.começo();
+						System.out.println("Menu de Carteira");
+						System.out.println("1 - Cadastrar\n"
+								+ "2 - Atualizar\n"
+								+ "3 - Remover\n"
+								+ "4 - Buscar\n"
+								+ "5 - Listar\n"
+								+ "6 - Sair\n");
+						int o = entrada.nextInt();
+						
+						switch(o) {
+							case 1:{
+							boolean ok = false;
+							do {
+								
+							} while (ok);
+							}
+						}
+					}
+				
 			
 			
 			
